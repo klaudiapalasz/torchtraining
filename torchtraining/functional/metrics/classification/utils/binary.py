9e5b619a4ca087ee42172aba8ccd7262e55d53a5
klaudiapalasz <82610195+klaudiapalasz@users.noreply.github.com>
2021-04-28 17:53:41 +0200

    Update binary.py

--- torchtraining/functional/metrics/classification/utils/binary.py
+++ torchtraining/functional/metrics/classification/utils/binary.py
@@ -11,12 +11,11 @@ def check(output: torch.Tensor, target: torch.Tensor) -> None:
 
     ValueError is raised otherwise
 
-    Parameters
-    ----------
-    output : torch.Tensor
-        Usually neural network output
-    target : torch.Tensor
-        Usually desired target
+    Arguments:
+        output : torch.Tensor:
+            Usually neural network output
+        target : torch.Tensor:
+            Usually desired target
 
     """
 
@@ -37,18 +36,16 @@ def threshold(
     For logits, `threshold` should be set to `0`, while for `probabilities`
     it should be `0.5`
 
-    Parameters
-    ----------
-    output : torch.Tensor
-        Usually neural network output
-    target : torch.Tensor
-        Usually desired target
-    threshold : float
-        Value at which `output`will be thresholded
+    Arguments:
+        output : torch.Tensor:
+            Usually neural network output
+        target : torch.Tensor:
+            Usually desired target
+        threshold : float
+            Value at which `output`will be thresholded
 
-    Returns
-    -------
-    typing.Tuple[torch.Tensor, torch.Tensor]:
-        Thresholded output and `target` being a boolean for faster computations.
+    Returns:
+        typing.Tuple[torch.Tensor, torch.Tensor]:
+            Thresholded output and `target` being a boolean for faster computations.
     """
     return output > threshold, target.bool()

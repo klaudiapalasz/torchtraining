d1d1293bfdb3817bfa74a44a139a10b000952326
klaudiapalasz <82610195+klaudiapalasz@users.noreply.github.com>
2021-04-28 18:28:40 +0200

    Update steps.py

--- torchtraining/utils/steps.py
+++ torchtraining/utils/steps.py
@@ -3,28 +3,30 @@ from . import docs_general
 
 def docstring(header, body):
     def gradient():
-        return r"""
-        gradient : bool
-            Whether to turn gradient on/off (for training/evaluation respectively).
+        return 
+        r"""
+            gradient :
+                Whether to turn gradient on/off (for training/evaluation respectively).
         """
 
     def device():
-        return r"""
-        device : torch.device
-            Device to which tensors could be casted. Available in `forward` as
-            `self.device`
+        return 
+        r"""
+            device :
+                Device to which tensors could be casted. Available in `forward` as
+                `self.device`
         """
 
     def wrapper(klass):
-        docstring = r"""{}.
+        docstring = 
+        r"""{}.
 
         {}
 
-        Parameters
-        ----------
-        criterion : typing.Callable
-            Criterion to use to get loss value. Available in `forward` as `self.criterion`
-            attribute.
+        Arguments:
+            criterion :
+                Criterion to use to get loss value. Available in `forward` as `self.criterion`
+                attribute.
         """.format(
             header, body
         )
